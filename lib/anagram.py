716cbd6ece5dc454eee11ff58d9a94ed44153591
Gachuri <rushionchegge@gmail.com>
2024-09-25 10:05:50 +0300

    tests passed, anagram implemented

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        """Initializes the Anagram instance with a single word."""
+        self.word = word
+
+    def match(self, word_list):
+        """Returns a list of words that are anagrams of the initialized word."""
+        sorted_word = sorted(self.word)
+        return [word for word in word_list if sorted(word) == sorted_word]
